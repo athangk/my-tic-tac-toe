7f169eb65a422a2144a8eceffe26082ee7a32125
Athanasios <41575222+athangk@users.noreply.github.com>
2021-10-03 21:10:37 +0300

    Changes order of null check

--- src/utilities/calculateWin.ts
+++ src/utilities/calculateWin.ts
@@ -7,8 +7,8 @@ const gameWon = (ticTacArray: string[]): boolean => {
 	return WIN_COMBINATION.some((combination) => {
 		return combination.every(
 			(el) =>
-				ticTacArray[el - 1] === ticTacArray[combination[0] - 1] &&
-				ticTacArray[el - 1] != null
+				ticTacArray[el - 1] != null && ticTacArray[el - 1] === ticTacArray[combination[0] - 1] 
+				
 		);
 	});
 };
