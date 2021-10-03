2bb795f7ee134ecdb38fd6dfc24625011926411f
Athanasios <41575222+athangk@users.noreply.github.com>
2021-10-03 21:32:13 +0300

    Updates classname of no marks to markNone

--- src/components/Board/BoardCell.tsx
+++ src/components/Board/BoardCell.tsx
@@ -10,7 +10,7 @@ export const BoardCell: FC<BoardCellProps> = (props) => {
 	return (
 		<div className="cell">
 			<div
-				className={`innerCell noSelect ${props.cellValue ? 'mark'+props.cellValue : null}`}
+				className={`innerCell noSelect mark${props.cellValue ? props.cellValue : 'None'}`}
 				onClick={() => props.handleClick(props.num)}
 			>
 				{props.cellValue}
