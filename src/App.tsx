b95740af43a6d82efdb69ff5f0c5da89796b2efb
athangk <me@me.gr>
2021-10-27 00:29:04 +0300

    Updated with automation test for game tie

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>I am here</div>
        <TicTacToe/>
       </header>
     </div>
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
-import {Board} from '../components/board/Board';
-import { BoardCell } from '../components/board/BoardCell';
-import { Player } from '../components/Player';
 
 test('Winner is O', () => {
 
@@ -50,6 +47,40 @@ test('Winner is X', () => {
   fireEvent.click(turn6)
 
   const linkElement = screen.getByText(/Winner: X/i);
+  expect(linkElement).toBeInTheDocument();
+  screen.debug();
+
+
+});
+
+
+
+test('Its a tie', () => {
+
+
+  const { container } = render(<App/>)
+
+  const turn1=container.getElementsByClassName('cell__inner')[1];
+  const turn2=container.getElementsByClassName('cell__inner')[0];
+  const turn3=container.getElementsByClassName('cell__inner')[3];
+  const turn4=container.getElementsByClassName('cell__inner')[2];
+  const turn5=container.getElementsByClassName('cell__inner')[4];
+  const turn6=container.getElementsByClassName('cell__inner')[5];
+  const turn7=container.getElementsByClassName('cell__inner')[8];
+  const turn8=container.getElementsByClassName('cell__inner')[7];
+  const turn9=container.getElementsByClassName('cell__inner')[6];
+ 
+  fireEvent.click(turn1)
+  fireEvent.click(turn2)
+  fireEvent.click(turn3)
+  fireEvent.click(turn4)
+  fireEvent.click(turn5)
+  fireEvent.click(turn6)
+  fireEvent.click(turn7)
+  fireEvent.click(turn8)
+  fireEvent.click(turn9)
+
+  const linkElement = screen.getByText(/Its a tie!/i);
   expect(linkElement).toBeInTheDocument();
   screen.debug();
 
