c7529657a1304946bbeaa0d9d1532a8e417848af
athangk <me@me.gr>
2021-12-11 10:16:14 +0200

    Change name of files

--- src/components/board/BoardCell.tsx
+++ src/components/board/BoardCell.tsx
@@ -1,15 +1,15 @@
 interface BoardCellProps {
   num: number
   cellValue: string | null
-  handleClick: (num: number) => void
+  handleClick: () => void
 }
 
-export function BoardCell({ num, cellValue, handleClick }: BoardCellProps) {
+export function BoardCell({ cellValue, handleClick }: BoardCellProps) {
   return (
     <div className="cell">
       <div
         className={`cell__inner no-select ${cellValue ? "mark-" + cellValue : "mark-none"}`}
-        onClick={() => handleClick(num)}
+        onClick={handleClick}
       >
         {cellValue}
       </div>
---
+++ src/utilities/333341.ts
@@ -0,0 +1,13 @@
+const WIN_COMBINATION = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [7, 5, 3],
+]
+const X_MARK = "X"
+const O_MARK = "O"
+export { WIN_COMBINATION, X_MARK, O_MARK }
